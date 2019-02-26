99895a6b1fd6b807f3eed5cf4bc1e4694819516d
Pavel <pavel@MacBook-Pro-2.local>
2019-02-26 14:42:32 +0300

    add image piker and adnroid isWork

--- android/app/src/main/java/com/rn/MainActivity.java
+++ android/app/src/main/java/com/rn/MainActivity.java
@@ -1,15 +1,40 @@
 package com.rn;
 
-import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback; // <- add this import
+import com.facebook.react.modules.core.PermissionListener; // <- add this import
+
+import com.reactnativecomponent.splashscreen.RCTSplashScreen;
+import com.reactnativenavigation.NavigationActivity;
+
+
+public class MainActivity extends NavigationActivity implements OnImagePickerPermissionsCallback{
+
+     private PermissionListener listener; 
+
+      @Override
+  public void setPermissionListener(PermissionListener listener)
+  {
+    this.listener = listener;
+  }
+
     @Override
-    protected String getMainComponentName() {
-        return "rn";
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+  {
+    if (listener != null)
+    {
+      listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //RCTSplashScreen.openSplashScreen(this);   //open splashscreen
+        RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.CENTER_CROP);   //open splashscreen fullscreen
+        super.onCreate(savedInstanceState);
     }
 }
